410311acbb4e01fb09638709d59b5111cdf8d572
Sarmad Mansoor <sarmadmansoorcheema@gmail.com>
2025-07-04 15:48:39 +0500

    first backend call

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,20 +1,47 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar'
+import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import Add from './pages/Add.jsx';
+import List from './pages/List.jsx';
+import Orders from './pages/Orders.jsx';
+import Login from './components/Login.jsx';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 const App = () => {
+  const [token, setToken] = useState(localStorage.getItem('token') || '');
+
   return (
-    <div className='bg-gray-50 min-h-screen'>
-      <>
-        <Navbar />
-        <hr/>
-        <div className='flex w-full'>
-            <Sidebar/>
-        </div>
-      </>
+    <div className='min-h-screen bg-gray-50'>
+      <ToastContainer />
+      {
+        token === '' ? (
+          <Login setToken={(token) => {
+            setToken(token);
+            localStorage.setItem('token', token);
+          }} />
+        ) : (
+          <>
+            <Navbar />
+            <hr />
+            <div className='flex w-full'>
+              <Sidebar />
+              <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+                <Routes>
+                  <Route path='/add' element={<Add />} />
+                  <Route path='/list' element={<List />} />
+                  <Route path='/orders' element={<Orders />} />
+                </Routes>
+              </div>
+            </div>
+          </>
+        )
+      }
     </div>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export const List = () => {
+const List = () => {
   return (
     <div>List</div>
   )
 }
+
+export default List
